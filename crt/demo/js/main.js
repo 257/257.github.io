c73fb1476766b1d3210116baf814d1ffb51b2f67
sgt. garcia <darwinskernel@gmail.com>
2015-05-14 14:11:40 -0400

    test: jqvintagetxt

--- crt/demo/js/main.js
+++ crt/demo/js/main.js
@@ -52,7 +52,6 @@ $(function() {
 "Image deployment           deploy studio",]
       ];
 
-
       self.el.vintageTxt('updateOptions', {
         textSpeed : 4
         ,linePause : 1024
@@ -60,16 +59,6 @@ $(function() {
         ,onFinishedTyping : null
         ,promptEnabled : false
       });
-var i;
-for(i=10; i>=0; i= i-1){
-   var s;
-   for(s=0; s<i; s = s+1){
-    document.write("*");
-   }
-   //i want this to print a new line
-   document.write(\n);
-
-};
       self.el.vintageTxt('playMany',texts);
     },
 
