fe58cf5128f7e35ccbd5d68402deaf4f219e7055
sgt. garcia <darwinskernel@gmail.com>
2015-05-14 13:59:55 -0400

    test: jqvintagetxt

--- crt/demo/js/main.js
+++ crt/demo/js/main.js
@@ -6,11 +6,11 @@ $(function() {
     go : function go() {
       this.el.vintageTxt({
          text : [
-["you asked for my resume? let me find that..."],
-["how are you btw?"
-"i'm running bits slow today just give us a sec..."
-"found it! that's a big one!"
-"running out of mem... there you go before that happens:"]
+"you asked for my resume? let me find that...",
+"how are you btw?",
+"i'm running bits slow today just give us a sec...",
+"found it! that's a big one!",
+"running out of mem... there you go before that happens:" 
         ]
         ,textSpeed: 16
         ,linePause: 128
@@ -27,14 +27,23 @@ $(function() {
 "Biology and Medical Sciences courses from University of Debrecen, Medical School",
 "\n",
 "EMPLOYMENT HISTORY",
-"Year         Employer                               Position/Notes",
-"1995-1996    [1] Kesahvarz F.C.                     Midfielder",
-"1996-1997    Bazyaft F.C.                           Midfielder",
-"1998-2003    Belal Electric Motor MFG Co.           IT Consultant",
-"1998-2003    Self-employed                          Tehran's Grand Bazaar",
-"2008-2010    School of Cinema Concordia Uni.        Post-Prod. Assistant, sysadmin",
-"2010-2011    Centre for Digital Arts Concordia Uni. *nix consultant, sysadmin",
-"2014-Current Pharex Canada                          network admin",]
+"Year                       Employer                               Position/Notes",
+"1995-1996                  [1] Kesahvarz F.C.                     Midfielder",
+"1996-1997                  Bazyaft F.C.                           Midfielder",
+"1998-2003                  Belal Electric Motor MFG Co.           IT Consultant",
+"1998-2003                  Self-employed                          Tehran's Grand Bazaar",
+"2008-2010                  School of Cinema Concordia Uni.        Post-Prod. Assistant, sysadmin",
+"2010-2011                  Centre for Digital Arts Concordia Uni. *nix consultant, sysadmin",
+"2014-Current               Pharex Canada                          network admin",
+"Fluent                     English, Farsi, Azerbaijani",
+"Elementary                 French",
+"Programming Languages      C, make, sh, python, Mathematica, R",
+"Open source collaborations openwrt, gentoo",
+"Certificates               Apple Certified Support Professional 10.5 (ACSP)",
+"Network                    DNS, DHCP, QoS, VoIP/sip, VPN, edgeos (vyatta/vyos fork) admin",
+"                           Currently am ISP to my own r\[u00E9]sidence (7 units plus Pharex Canada)",
+"Directory services         Open Directory (OSX version of openldap), kerborized triangle",
+"Image deployment           deploy studio",]
       ];
 
       self.el.vintageTxt('updateOptions', {
