3c11ad133ff7703f59837993caca2e59e5f890c4
sgt. garcia <darwinskernel@gmail.com>
2015-05-14 15:08:02 -0400

    test: jqvintagetxt

--- crt/demo/js/main.js
+++ crt/demo/js/main.js
@@ -23,8 +23,8 @@ $(function() {
     edu : function edu() {
     var fill_screen = Array(32).join(".");
     var texts = [
-[fill_screen],
-["EDUCATION",
+fill_screen,
+"EDUCATION",
 "BA with Distinction in Mathematics and Statistics (Graduated April 2014)",
 "Biology and Medical Sciences courses from University of Debrecen, Medical School",
 "\n",
@@ -51,8 +51,7 @@ $(function() {
 "Network                    DNS, DHCP, QoS, VoIP/sip, VPN, edgeos (vyatta/vyos fork) admin",
 "                           Currently am ISP to my own résidence (7 units plus Pharex Canada)",
 "Directory services         Open Directory (OSX version of openldap), kerborized triangle",
-"Image deployment           deploy studio"]
-      ];
+"Image deployment           deploy studio"];
 
       self.el.vintageTxt('updateOptions', {
         textSpeed : 4
