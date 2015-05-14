d64d08fa21cfcb56d2ed067affbbff5551ec05c5
sgt. garcia <darwinskernel@gmail.com>
2015-05-14 11:37:09 -0400

    test: jqvintagetxt

--- crt/demo/js/main.js
+++ crt/demo/js/main.js
@@ -16,9 +16,10 @@ $(function() {
 
     intro : function intro() {
       var texts = [
-"EDUCATION\n",\
-"BA with Distinction in Mathematics and Statistics (Graduated April 2014)\n",\
-"Biology and Medical Sciences courses from University of Debrecen, Medical School\n"];
+"EDUCATION\n",
+"BA with Distinction in Mathematics and Statistics (Graduated April 2014)\n",
+"Biology and Medical Sciences courses from University of Debrecen, Medical School\n"
+];
 
       self.el.vintageTxt('updateOptions', {
         textSpeed: 60
