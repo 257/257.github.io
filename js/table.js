2c33c70fd561b06e11c04f35443074b5fa59fc4b
sgt. garcia <darwinskernel@gmail.com>
2015-05-16 22:36:32 -0400

    test: typed.js

--- js/table.js
+++ js/table.js
@@ -1,4 +1,4 @@
-function gen_resume(){
+function gen_resume() {
   var x = document.createElement("TABLE");
   x.setAttribute("id", "career");
   document.body.appendChild(x);
@@ -13,6 +13,41 @@ function gen_resume(){
   document.getElementById("95-96").appendChild(z);
   return x;
 }
-function say_hello(){
+function say_hello() {
   return ["hellp world"];
 }
+
+function generate_table() {
+  // get the reference for the body
+  var body = document.getElementsByTagName("body")[0];
+ 
+  // creates a <table> element and a <tbody> element
+  var tbl     = document.createElement("table");
+  // var tblHead = document.createElement("thead");
+  var tblBody = document.createElement("tbody");
+ 
+  // creating all cells
+  for (var i = 0; i < 2; i++) {
+    // creates a table row
+    var row = document.createElement("tr");
+ 
+    for (var j = 0; j < 2; j++) {
+      // Create a <td> element and a text node, make the text
+      // node the contents of the <td>, and put the <td> at
+      // the end of the table row
+      var cell = document.createElement("td");
+      var cellText = document.createTextNode("cell in row "+i+", column "+j);
+      cell.appendChild(cellText);
+      row.appendChild(cell);
+    }
+ 
+    // add the row to the end of the table body
+    tblBody.appendChild(row);
+  }
+ 
+  // put the <tbody> in the <table>
+  tbl.appendChild(tblBody);
+  // appends <table> into <body>
+  body.appendChild(tbl);
+  // sets the border attribute of tbl to 2;
+  tbl.setAttribute("border", "2");
