5aa6a20f29d2b5644b711a5008d2aadde54e7b1b
sgt. garcia <darwinskernel@gmail.com>
2015-05-16 20:08:08 -0400

    test: typed.js

---
+++ js/table.js
@@ -0,0 +1,17 @@
+<script>
+  function gen_resume() {
+    var x = document.createElement("TABLE");
+    x.setAttribute("id", "career");
+    document.body.appendChild(x);
+
+    var y = document.createElement("TR");
+    y.setAttribute("id", "95-96");
+    document.getElementById("career").appendChild(y);
+
+    var z = document.createElement("TD");
+    var t = document.createTextNode("1995-1996");
+    z.appendChild(t);
+    document.getElementById("95-96").appendChild(z);
+    return x;
+  }
+</script>
