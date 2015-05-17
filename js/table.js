da0db4978c92a915c45a789d32035efe275ac224
sgt. garcia <darwinskernel@gmail.com>
2015-05-16 20:56:29 -0400

    test: typed.js

--- js/table.js
+++ js/table.js
@@ -1,17 +1,20 @@
 <script>
-  function gen_resume() {
-    var x = document.createElement("TABLE");
-    x.setAttribute("id", "career");
-    document.body.appendChild(x);
+function gen_resume() {
+  var x = document.createElement("TABLE");
+  x.setAttribute("id", "career");
+  document.body.appendChild(x);
 
-    var y = document.createElement("TR");
-    y.setAttribute("id", "95-96");
-    document.getElementById("career").appendChild(y);
+  var y = document.createElement("TR");
+  y.setAttribute("id", "95-96");
+  document.getElementById("career").appendChild(y);
 
-    var z = document.createElement("TD");
-    var t = document.createTextNode("1995-1996");
-    z.appendChild(t);
-    document.getElementById("95-96").appendChild(z);
-  }
-gen_resume();
+  var z = document.createElement("TD");
+  var t = document.createTextNode("1995-1996");
+  z.appendChild(t);
+  document.getElementById("95-96").appendChild(z);
+}
+function say_hello(){
+  return 'hellp world'
+
+}
 </script>
