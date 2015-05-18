39868044af91f231d9b8d43845ba86fd8e13e538
sgt. garcia <darwinskernel@gmail.com>
2015-05-18 13:35:07 -0400

    test: typed.js

--- js/test.js
+++ js/test.js
@@ -2,7 +2,31 @@ function say_hello() {
  document.write("hello world");
 }
 
-<script src="../js/vars.js"></script>
+var tbl_man = "\
+  EMPLOYMENT HISTORY|\
+  Year:Employer:Position/Notes|\
+  1995-1996:Kesahvarz F.C.:Midfielder|\
+  1996-1997:Bazyaft F.C.:Midfielder|\
+  1998-2003:Belal Electric Motor MFG Co.:IT Consultant|\
+  1998-2003:Self-employed:Tehran's Grand Bazaar|\
+  2008-2010:School of Cinema Concordia Uni.:Post-Prod. Assistant, sysadmin|\
+  2010-2011:Centre for Digital Arts Concordia Uni.:*nix consultant, sysadmin|\
+  2014-Current:Pharex Canada:network admin|\
+  \ |\
+  SPOKEN LANGUAGES|\
+  Fluent:English, Farsi, Azerbaijani|\
+  Elementary:French|\
+  PROGRAMMING LANGUAGES, TECHNICAL CERTIFICATES, etc|\
+  Programming Languages:C, make, sh, python, Mathematica, R|\
+  Open source collaborations:openwrt, gentoo|\
+  Certificates:Apple Certified Support Professional 10.5 (ACSP)|\
+  Server Administration|\
+  Network:DNS, DHCP, QoS, VoIP/sip, VPN, edgeos (vyatta/vyos fork) admin|\
+  :Currently am ISP to my own r\[u00E9]sidence (7 units plus Pharex Canada)|\
+  Directory services:Open Directory (OSX version of openldap), kerborized triangle|\
+  Image deployment:deploy studio|\
+  CONTACT INFO.|\
+  Email:<darwinskernel at gmail.com>"
 
 function generate_table() {
   var lines = tbl_man.split('|');
