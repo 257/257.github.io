a4e5f4cc9a347a5f054ce5b5a8ebc3c1462a8ab8
sgt. garcia <darwinskernel@gmail.com>
2015-05-18 11:17:51 -0400

    test: typed.js

--- js/test.js
+++ js/test.js
@@ -59,5 +59,5 @@ function generate_table() {
   /* body.appendChild(tbl); */
   // sets the border attribute of tbl to 2;
   /* tbl.setAttribute("border", "1"); */
-  return tbl.write();
+  return $(tbl[0])[0].outerHTML;
 }
