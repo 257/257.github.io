179d6451979a09ab019d94a6ac51f0759643bc7a
jrun <darwinskernel@gmail.com>
2020-07-24 15:42:16 -0400

    slow down

--- assets/demos.js
+++ assets/demos.js
@@ -1,11 +1,13 @@
 var conf = {
-        typeSpeed : 0,
+        typeSpeed : fspeed,
         smartBackspace : false,
         cursorChar : '&#x2588;',
-        typeSpeed : 0,
+        typeSpeed : fspeed,
         backSpeed : 0,
         loop : false,
 };
+
+var fspeed = 96;
 
 document.addEventListener('DOMContentLoaded', function()
         {
@@ -24,7 +26,6 @@ document.addEventListener('DOMContentLoaded', function()
                                 onComplete: function(self) {
                                         prettyLog('onComplete ' + self);
                                         self.destroy();
-                                        edu();
                                 },
                                 preStringTyped: function(pos, self)
                                 {
@@ -59,26 +60,23 @@ document.addEventListener('DOMContentLoaded', function()
                                 },
                                 onDestroy: function(self) {
                                         prettyLog('onDestroy ' + self);
+                                        edu();
                                 }
                         })
         })
 
 function edu()
 {
-        var str = [
-                "`EDUCATION\n`" +
-                "`[April 2014] Paymon redceived his Bachelors in Mathematics and Statistics, awarded with distinction`" +
-                "`\n`"
-        ];
+        var str = "`EDUCATION\n" +
+                "`[April 2014] Paymon redceived his Bachelors in Mathematics and Statistics, awarded with distinction ``\n`"
 
         new Typed('#edu', {
-                typeSpeed : 0,
                 smartBackspace : false,
                 cursorChar : '&#x2588;',
-                typeSpeed : 0,
+                typeSpeed : 16,
                 backSpeed : 0,
                 loop : false,
-                strings : str,
+                strings : [ str ],
                 onComplete: function(self)
                 {
                         prettyLog('onComplete ' + self);
@@ -90,20 +88,17 @@ function proglangs()
 {
           var str =
                 "`PROGRAMMING LANGUAGES`\n" +
-                "`Paymon is mainly a system programmer. He codes mostly in C and sometimes C++ " +
-                "`writing threaded, realtime applicaton, server daemons\n`" +
-                "`He also has experience coding in python, php, sh, R, lua, perl postgres, mysql, javascript, etc\n`" +
-                "`In the past he has taken on the role of administration of realtime applicatons (carrier telephony), running in containers, " +
-                "VMs (xen, qemu and KVM) and in production environment\n`" +
-                "Kernel's Network Stack is what he likes to fiddle with, .\n"
-                "`Build systems and pkg managers he's deals with on a day-to-day basis are\n`" +
-                "`\tKernel Build System (Kconfig), gentoo (portage), openwrt (buildroot)`" +
-                "`\n`";
+                "`Paymon is mainly a system programmer. He codes mostly in C and sometimes C++ writing threaded, realtime applicaton, server daemons`\n" +
+                "`He also has experience coding in python, php, sh, R, lua, perl postgres, mysql, javascript, etc`\n" +
+                "`In the past he has taken on the role of administration of realtime applicatons (carrier telephony)," +
+                " running in containers, VMs (xen, qemu and KVM) and in production environment`\n" +
+                "`Kernel's Network Stack is what he likes to fiddle with.`\n" +
+                "`Build systems and pkg managers he's deals with on a day-to-day basis are`\n" +
+                "`\tKernel Build System (Kconfig), gentoo (portage), openwrt (buildroot)`\n";
         new Typed('#proglangs', {
-                typeSpeed : 0,
-                smartBackspace : false,
+                smartBackspace : true,
                 cursorChar : '&#x2588;',
-                typeSpeed : 0,
+                typeSpeed : fspeed,
                 backSpeed : 0,
                 loop : false,
                 strings : [ str ],
@@ -120,12 +115,11 @@ function work()
                 "`WORK EXPERIENCE\n`" +
                 "`[Jan 2010 - Aug 2011] Paymon worked System Programmer/Admin at CDA (Center for Digital Art)`\n" +
                 "`[May 2014 - May 2016] Paymon did IT Consultant, he was not happy doing that!`\n" +
-                "`[May 2016 - Jul 2019] Paymon worked at NuNetworx Inc. as System/Network Developer/Administrator\n`" +
-                "`[Nov 2019 -  Current] Paymon currenty works at Savoir Faire Linux as part of Jami's core development team`" +
-                "`\n`";
+                "`[May 2016 - Jul 2019] Paymon worked at NuNetworx Inc. as System/Network Developer/Administrator`\n" +
+                "`[Nov 2019 -  Current] Paymon currenty works at Savoir Faire Linux as part of Jami's core development team\n`";
 
         new Typed('#work', {
-                typeSpeed : 0,
+                typeSpeed : fspeed,
                 smartBackspace : false,
                 cursorChar : '&#x2588;',
                 typeSpeed : 128,
@@ -142,18 +136,18 @@ function work()
 function misc()
 {
         var str =
-                "`MISCELLANEOUS\n`" +
-                "`Paymon spkeas Fluent Engligh and  Advanced Intermediate French\n`" +
-                "`Paymon's mother tongue is Farsi, Azerbaijani\n" +
-                "`Paymon also liks Cognitive Science, Philosophy, Linguistics, Economics and System Theory\n" +
-                "`He _liks_ to think he is an avid Rock Climber (he's not), a Cyclist (possibly) and an *ok* Swimmer (that's a joke).\n" +
-                "`He Played [Association] Football professionally until the age of 18 when he had to stop because of injury.`" +
-                "`\n`";
+                "`MISCELLANEOUS`\n" +
+                "`Paymon spkeas Fluent Engligh and  Advanced Intermediate French`\n" +
+                "`Paymon's Native Languages are Farsi, Azerbaijani`\n" +
+                "`Paymon also liks Cognitive Science, Philosophy, Linguistics, Economics and System Theory`\n" +
+                "`He _liks_ to think he is an avid Rock Climber (he's not), a Cyclist (possibly) and an *ok* Swimmer (that's a joke).`\n" +
+                "`He Played [Association] Football professionally until the age of 18 when he had to stop because of injury.`\n";
+
         new Typed('#misc', {
-                typeSpeed : 0,
+                typeSpeed : fspeed,
                 smartBackspace : false,
                 cursorChar : '&#x2588;',
-                typeSpeed : 0,
+                typeSpeed : fspeed,
                 backSpeed : 0,
                 loop : false,
                 strings : [ str ],
@@ -167,51 +161,51 @@ function misc()
 function contrib()
 {
         var str =
-                "`OPEN SOUCE CONTRIBUTIONS\n`" +
-                "`Paymon has following open source contributions\n`" +
-                "`- CMake\n`" +
+                "`OPEN SOUCE CONTRIBUTIONS`\n" +
+                "`Paymon has following open source contributions`\n" +
+                "`- CMake`\n" +
                 "`\t<a href=" +
                 "https://gitlab.kitware.com/cmake/cmake/-/commit/3211e07dc6f2b52570f7b9e1034a2e0394c83862" +
                 ">" +
                 "Ncurses Module" +
-                "</a>\n`" +
-                "`- pjsip\n`" +
+                "</a>`\n" +
+                "`- pjsip`\n" +
                 "`\t<a href=" +
                 "https://github.com/pjsip/pjproject/commit/645611d8a985a03f7509ec1455ed3de4a2e12ebb" +
                 ">" +
                 "Implementation of on_valid_pair() in ICE STREAM TRANSPORT" +
-                "</a>\n`" +
+                "</a>`\n" +
                 "`\t<a href=" +
                 "https://github.com/pjsip/pjproject/pull/2363" +
                 ">" +
                 "Collaboration of implementation of RFC 6544" +
-                "</a>\n`" +
+                "</a>`\n" +
                 "`\t<a href=" +
                 "https://github.com/savoirfairelinux/opendht/commit/9487bc5e422c64735f69ccc303636bc2124027be" +
                 ">" +
                 "Implementation of connectivity change in peer discovery module of opendht" +
-                "</a>\n`" +
+                "</a>`\n" +
                 "`\t<a href=" +
                 "'https://github.com/savoirfairelinux/opendht/compare/master...257:connstat'" +
                 ">" +
                 "Implementation of ConnectivityChange() in linux based OSes to work directly with kernel via Netlink" +
-                "</a>\n`" +
+                "</a>`\n" +
                 "`<a href=" +
                 "https://review.jami.net/q/author:jrun" +
                 ">" +
                 "- Jami contributions" +
-                "</a>\n`" +
+                "</a>`\n" +
                 "`<a href=" +
                 "https://github.com/257" +
                 ">" +
                 "- Contributions on github" +
-                "</a>\n`";
+                "</a>``\n`";
 
         new Typed('#contrib', {
-                typeSpeed : 0,
+                typeSpeed : fspeed,
                 smartBackspace : false,
                 cursorChar : '&#x2588;',
-                typeSpeed : 0,
+                typeSpeed : fspeed,
                 backSpeed : 0,
                 loop : false,
                 strings : [ str ],
